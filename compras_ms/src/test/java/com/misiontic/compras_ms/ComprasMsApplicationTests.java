e2bdae18ba93cf6d3d958b44246c6b44298498e9
juansuarezr <juan.suarezr@udea.edu.co>
2021-08-15 14:05:48 -0500

    Configuración inicial del proyecto

---
+++ compras_ms/src/test/java/com/misiontic/compras_ms/ComprasMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.compras_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
